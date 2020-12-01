7b4d1c78a72b20fdc5893fcad24c9a42e2e51966
Hao <haoyang439@gmail.com>
2020-12-01 14:16:35 -0700

    MyFix of Issue3

--- StoreApplication/StoreApplication.UnitTests/StoreRepositoryTest.cs
+++ StoreApplication/StoreApplication.UnitTests/StoreRepositoryTest.cs
@@ -14,25 +14,29 @@ namespace StoreApplication.UnitTests
     public class StoreRepositoryTest
     {
 
-        static DbContextOptions<Project0databaseContext> option;
-
+        // static DbContextOptions<Project0databaseContext> option;
+        // using actual Azure database only for these tests , but use SQLite for other tests
         [Fact]
         public void DBAddAStore()
         {
             // setup
             var optionsBuilder = new DbContextOptionsBuilder<Project0databaseContext>();
             optionsBuilder.UseSqlServer(GetConnectionString());
-            option = optionsBuilder.Options;
-
-            IStoreRepository repo = new StoreRepository(option);       
+            var option = optionsBuilder.Options;
             CStore newStore = new CStore("Mountain View 1", "6026626662");
 
-            // action
-            repo.AddOneStore(newStore);
+            using (var context1 = new Project0databaseContext(option))
+            {
+                // using different context 
+                IStoreRepository repo = new StoreRepository(option);
+
+                // action
+                repo.AddOneStore(newStore);
+            }
 
             // asert
-            using var context = new Project0databaseContext(option);
-            var dbStore = context.Stores.First(x => x.Storeloc == "Mountain View 1");
+            using var context2 = new Project0databaseContext(option);
+            var dbStore = context2.Stores.First(x => x.Storeloc == "Mountain View 1");
             Assert.Equal(newStore.Storephone, dbStore.Storephone);
             Assert.Empty(dbStore.Storecustomers);
         }
@@ -41,29 +45,45 @@ namespace StoreApplication.UnitTests
         public void DBAddOneProduct() {
             var optionsBuilder = new DbContextOptionsBuilder<Project0databaseContext>();
             optionsBuilder.UseSqlServer(GetConnectionString());
-            option = optionsBuilder.Options;
+            var option = optionsBuilder.Options;
+            CProduct newProduct = new CProduct("Product101", "Duck", "Meat", 10.0);
+            using (var context1 = new Project0databaseContext(option))
+            {
+                IStoreRepository repo = new StoreRepository(option);
+                // action
+                repo.AddOneProduct(newProduct);
+            }
 
-            IStoreRepository repo = new StoreRepository(option);
-            CProduct newProduct = new CProduct("Product101", "Duck","Meat",10.0);
-
-            // action
-            repo.AddOneProduct(newProduct);
-
-            // asert
-            using var context = new Project0databaseContext(option);
-            var dbProduct = context.Products.Find("Product101");
-
+            // assert
+            using var context2 = new Project0databaseContext(option);
+            var dbProduct = context2.Products.Find("Product101");
             Assert.Equal(newProduct.Name, dbProduct.Name);
             Assert.Equal(newProduct.Category, dbProduct.Category);
             Assert.Equal(newProduct.Price, dbProduct.Price);           
         }
 
-        public void DBAddOneCustomer()
+        [Fact]
+        public void DBGetAProductByNameAndCategory()
         {
-            
-        
-        }
+            var optionsBuilder = new DbContextOptionsBuilder<Project0databaseContext>();
+            optionsBuilder.UseSqlServer(GetConnectionString());
+            var option = optionsBuilder.Options;
+            string name = "Duck";
+            string category = "Meat";
+            double price = 10.0;
+            using (var context1 = new Project0databaseContext(option))
+            {
+                IStoreRepository repo = new StoreRepository(option);
+                // action
+                CProduct p = repo.GetAProductByNameAndCategory(name, category);
+            }
 
+            // assert
+            using var context2 = new Project0databaseContext(option);
+            var dbProduct = context2.Products.Find("Product101");
+            Assert.Equal(price, dbProduct.Price);
+            
+        }
 
         static string GetConnectionString()
         {
